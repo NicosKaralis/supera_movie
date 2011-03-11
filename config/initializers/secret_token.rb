54ffc9608fb36e59fb1c7072a38b9f9f186f8fe1
Nicos Karalis <dymytrhy@gmail.com>
2011-03-11 16:12:59 -0300

    Rails squeleton added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperaMovie::Application.config.secret_token = '357f9df1283cd22de5815f8238b0ac5cd2dc03947eeb45cb35ef9a3dd0067d56ab7da207153b7295fd52528233a57a0fa374941669969934cd33fb35f073ead4'
